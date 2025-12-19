834e3e805131d1ad0bbe3ccee682630b45a3237c
Arshia <arshia.tehrani1380@gmail.com>
2025-12-18 19:01:30 -0500

    Remove local_run.bat and replace it with local_run.txt for streamlined training command configuration. Updated batch size and number of workers for improved performance.

--- local_run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Activate virtual environment and run training with maximum GPU and CPU utilization
-call venv\Scripts\activate.bat
-python train.py --model Conv --dataloader MNIST --data_dir ./data --batch_size 256 --epoch 20 --gpus -1 --num_workers -1 --logdir ./logs --lr 0.001
-pause
-
